994eec49fc4bd83f6e8fe2b6c73a24275cbba8d1
Inka Riyadi <inka.riyadi@gmail.com>
2021-09-19 16:55:55 +0700

    add utils for printing layers

---
+++ utils.py
@@ -0,0 +1,33 @@
+## Python program to print the data
+
+def sum_parameter(d):
+  sum_params = 0
+  for k, v in d.items():
+    layer, shape, param = v
+    sum_params += param
+  
+  return sum_params
+
+def print_layer(d):
+  print()
+  print("———————————————————————————————————————————————————————————————————————")
+  print("{:<30} {:<30} {:<10}".format('Layer (type) ','Output Shape','Param #'))
+  print("=======================================================================")
+  for k, v in d.items():
+    lang, perc, change = v
+    print("{:<30} {:<30} {:<10}".format(lang, perc, change))
+    if (k != list(d.keys())[-1]): 
+      print("———————————————————————————————————————————————————————————————————————")
+    else:
+      print("=======================================================================")
+
+  print("Total Params: {}".format(sum_parameter(d)))
+  print("Trainable Params: {}".format(sum_parameter(d)))
+  print("Non-trainable Params: {}".format(sum_parameter(d)))
+  print()
+
+d = {1: ["conv2d (Conv2D)", "(None, 123, 123, 32)", 2432],
+  2: ["conv2d_1 (Conv2D)", "(None, 121, 121, 32)", 9248],
+  3: ["max_pooling2d (MaxPooling2D) ", "(None, 40, 40, 32)", 0]}
+  
+print_layer(d)
