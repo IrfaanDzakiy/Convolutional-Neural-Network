fix softmax range error

dense_layer.py
@@ -17,7 +17,8 @@ class DenseLayer:
 
     def generate_weight(self):
 
-        return [np.random.random(size=len(self.flattened_input)) for i in range(self.unit)]
+        # return [np.random.uniform(100, 1000, len(self.flattened_input)) for i in range(self.unit)]
+        return [np.random.randint(10, size=len(self.flattened_input)) for i in range(self.unit)]
 
     def set_activation_function(self, activation_function):
         self.activation_function = activation_function
@@ -42,6 +43,7 @@ class DenseLayer:
 
         output = []
         activated_output = []
+        
         self.set_input(inputs)
 
         for i in range(self.unit):

utils.py
@@ -14,10 +14,10 @@ def softmax(input):
     result = []
     sum_of_exp_z = 0
     for i in range(len(input)):
-        sum_of_exp_z += math.exp(input[i])
+        sum_of_exp_z += np.exp(input[i])
 
     for i in range(len(input)):
-        result.append(math.exp(input[i]) / sum_of_exp_z)
+        result.append(np.exp(input[i]) / sum_of_exp_z)
     return result
 
 