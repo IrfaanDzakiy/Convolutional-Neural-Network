fix pooling backprop

convolution_layer.py
@@ -222,18 +222,23 @@ class PoolingStage:
         dL_dInput = np.zeros(paddedInput.shape)
 
         for i_dOut in range(n_output):
-            for i in range(0, size_output, self.stride):
-                for j in range(0, size_output, self.stride):
+            for i in range(0, size_output):
+                for j in range(0, size_output):
+                    rec_i_start = i * self.stride
+                    rec_i_end = rec_i_start + self.filter_size
+                    rec_j_start = j * self.stride
+                    rec_j_end = rec_j_start + self.filter_size
+
                     if (self.mode == MAX):
-                        rec_field = paddedInput[i_dOut, i:i +
-                                                self.filter_size, j:j + self.filter_size]
+                        rec_field = paddedInput[i_dOut,
+                                                rec_i_start:rec_i_end, rec_j_start:rec_j_end]
                         (x_max, y_max) = np.argwhere(
                             rec_field == dL_dOut[i_dOut, i, j])[0]
-                        dL_dInput[i_dOut, i + x_max, j +
+                        dL_dInput[i_dOut, rec_i_start + x_max, rec_j_start +
                                   y_max] = dL_dOut[i_dOut, i, j]
                     elif (self.mode == AVERAGE):
-                        dL_dInput[i_dOut, i:i + self.filter_size, j:j +
-                                  self.filter_size] += dL_dOut[i_dOut, i, j]/(self.filter_size**2)
+                        dL_dInput[i_dOut, rec_i_start:rec_i_end,
+                                  rec_j_start:rec_j_end] += dL_dOut[i_dOut, i, j]/(self.filter_size**2)
 
         if (self.padding > 0):
             dL_dInput = dL_dInput[:, self.padding:-

main.py
@@ -14,9 +14,10 @@ mat_example = [
 inputs = np.random.randint(
     256, size=(1, 32, 32))
 
+
 def main():
     inputs = extract_mnist_images("train-images-idx3-ubyte", 2)
-    
+
     for image in inputs:
         convoLayer = ConvolutionLayer((3, 2), RELU, MAX, 2)
         denseLayer = DenseLayer(120, "sigmoid")
@@ -27,6 +28,7 @@ def main():
         model.add_layer(denseLayer)
         model.calculate()
         model.print_summary()
+
 
 if __name__ == '__main__':
     main()

utils.py
@@ -100,27 +100,27 @@ def convert_grayscale_to_rgb(image=None):
     return stacked_img
 
 
-# Test k-cross validation , remove this later
-dataset = np.arange(90).reshape((10, 3, 3))
-test, train = cross_validation(10, dataset, 2)
-print(test)
-print(train)
+if __name__ == '__main__':
+    # Test k-cross validation , remove this later
+    dataset = np.arange(90).reshape((10, 3, 3))
+    test, train = cross_validation(10, dataset, 2)
+    print(test)
+    print(train)
 
-# Test accuracy , remove this later
-true_values = np.array([[1, 0, 0, 1, 1, 1, 1, 1, 1, 0]])
-predictions = np.array([[1, 0, 0, 1, 1, 1, 1, 1, 0, 1]])
-print(accuracy(true_values, predictions))
+    # Test accuracy , remove this later
+    true_values = np.array([[1, 0, 0, 1, 1, 1, 1, 1, 1, 0]])
+    predictions = np.array([[1, 0, 0, 1, 1, 1, 1, 1, 0, 1]])
+    print(accuracy(true_values, predictions))
 
+    arr = extract_mnist_images("train-images-idx3-ubyte", 2)
+    print(arr)
+    print(arr.shape)
 
-arr = extract_mnist_images("train-images-idx3-ubyte", 2)
-print(arr)
-print(arr.shape)
+    stacked_arr = convert_grayscale_to_rgb(arr)
+    print(stacked_arr.shape)
+    print(stacked_arr[0].shape)
 
-stacked_arr = convert_grayscale_to_rgb(arr)
-print(stacked_arr.shape)
-print(stacked_arr[0].shape)
-
-# Test convert grayscale to RGB
-for i in stacked_arr:
-    img = Image.fromarray(i, 'RGB')
-    img.show()
+    # Test convert grayscale to RGB
+    for i in stacked_arr:
+        img = Image.fromarray(i, 'RGB')
+        img.show()